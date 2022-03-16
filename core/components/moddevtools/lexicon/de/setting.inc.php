c5389cd5a0f5fb3f4845731610c90edb73fe7890
Thomas Jakobi <buero@treehillstudio.de>
2022-03-16 14:46:57 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/MODX modDevTools/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-moddevtools-system-settings/de/

--- core/components/moddevtools/lexicon/de/setting.inc.php
+++ core/components/moddevtools/lexicon/de/setting.inc.php
@@ -5,9 +5,9 @@
  * @package moddevtools
  * @subpackage lexicon
  */
-$_lang['setting_moddevtools.breadcrumb_limit'] = 'Breadcrumb Limit';
-$_lang['setting_moddevtools.breadcrumb_limit_desc'] = 'Limit of displayed breadcrumb entries.';
+$_lang['setting_moddevtools.breadcrumb_limit'] = 'Breadcrumb-Limit';
+$_lang['setting_moddevtools.breadcrumb_limit_desc'] = 'Limit der angezeigten Breadcrumb-Einträge.';
 $_lang['setting_moddevtools.debug'] = 'Debug';
 $_lang['setting_moddevtools.debug_desc'] = 'Log debug information in the MODX error log.';
-$_lang['setting_moddevtools.show_breadcrumb_context'] = 'Show Breadcrumb Context';
-$_lang['setting_moddevtools.show_breadcrumb_context_desc'] = 'Show the context key in the breadcrumb path.';
+$_lang['setting_moddevtools.show_breadcrumb_context'] = 'Breadcrumb-Kontext anzeigen';
+$_lang['setting_moddevtools.show_breadcrumb_context_desc'] = 'Zeigt den Kontextschlüssel im Breadcrumb-Pfad an.';
