db95467a98dbe987f508d13e4ab03790e392ff09
Thomas Jakobi <buero@treehillstudio.de>
2025-08-28 14:42:23 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/moddevtools/system-settings/ru/

--- core/components/moddevtools/lexicon/ru/setting.inc.php
+++ core/components/moddevtools/lexicon/ru/setting.inc.php
@@ -8,6 +8,6 @@
 $_lang['setting_moddevtools.breadcrumb_limit'] = 'Лимит хлебных крошек';
 $_lang['setting_moddevtools.breadcrumb_limit_desc'] = 'Лимит отображаемых записей хлебных крошек.';
 $_lang['setting_moddevtools.debug'] = 'Отладка';
-$_lang['setting_moddevtools.debug_desc'] = 'Записывать отладочную информацию в журнал ошибок MODX.';
+$_lang['setting_moddevtools.debug_desc'] = 'Записывать отладочную информацию в лог ошибок MODX.';
 $_lang['setting_moddevtools.show_breadcrumb_context'] = 'Показать контекст навигационной цепочки';
 $_lang['setting_moddevtools.show_breadcrumb_context_desc'] = 'Показать ключ контекста в пути хлебных крошек.';
