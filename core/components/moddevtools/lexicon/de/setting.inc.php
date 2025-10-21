ebdfa0be68866a22c1656409927fbc2d7c135758
Thomas Jakobi <buero@treehillstudio.de>
2025-10-21 19:50:09 +0200

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/moddevtools/system-settings/de/

--- core/components/moddevtools/lexicon/de/setting.inc.php
+++ core/components/moddevtools/lexicon/de/setting.inc.php
@@ -8,6 +8,6 @@
 $_lang['setting_moddevtools.breadcrumb_limit'] = 'Breadcrumb-Limit';
 $_lang['setting_moddevtools.breadcrumb_limit_desc'] = 'Limit der angezeigten Breadcrumb-Einträge.';
 $_lang['setting_moddevtools.debug'] = 'Debug';
-$_lang['setting_moddevtools.debug_desc'] = 'Log debug information in the MODX error log.';
+$_lang['setting_moddevtools.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
 $_lang['setting_moddevtools.show_breadcrumb_context'] = 'Breadcrumb-Kontext anzeigen';
 $_lang['setting_moddevtools.show_breadcrumb_context_desc'] = 'Zeigt den Kontextschlüssel im Breadcrumb-Pfad an.';
